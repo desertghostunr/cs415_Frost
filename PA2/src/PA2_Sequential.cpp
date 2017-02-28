8951838b5204830a5ce085a022fa590fd1ed39de
= <=>
2017-02-28 12:25:41 -0800

    Started work on Sequential code.

--- PA2/src/PA2_Sequential.cpp
+++ PA2/src/PA2_Sequential.cpp
@@ -18,6 +18,11 @@
 // header files //////////////////////////////////////////////
 
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include "PIMFuncs.h"
+#include "Mandelbrot.h"
 
 //free function prototypes ///////////////////////////////////
 unsigned long long GetCurrentMicroSecTime( );
@@ -27,7 +32,42 @@ double ConvertTimeToSeconds( unsigned long long usTime );
 // main /////////////////////////////////////////////////////
 int main( int argc, char *argv[ ] )
 {
+    unsigned long long sTime, eTime;
+    std::vector<std::vector<char>> image; 
+    size_t row, col;
+    std::stringstream strStream;
+    std::string saveName;
+    mb::ComplexNumber cNum;
+
+    if( argc < 4 )
+    {
+        std::cout << "The program must be ran with the following:" <<std::endl;
+        std::cout << "./PA2_Sequential [number of columns] [number of rows] [file path name]" <<std::endl;
+        return -1;
+    }
     
+    strStream.str( ) = argv[ 2 ];
+    strStream >> col;
+
+    strStream.str( ) = argv[ 3 ];
+    strStream >> row;
+
+    saveName = argv[ 4 ];
+
+    image.resize( row );
+
+    for( row = 0; row < image.size( ); row++ )
+    {
+        image[ row ].resize( col );
+    }
+
+    for( row = 0; row < image.size( ); row++ )
+    {
+        for( col = 0; col < image[ row ].size( ); col++ )
+        {
+            
+        }
+    }
     
 
     return 0;
