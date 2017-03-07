170e09763781de22af3f1b5cf181b66e023d8f61
= <=>
2017-03-07 15:50:30 -0800

    Started fixing runtests thing

--- PA2/build/RunTests.sh
+++ PA2/build/RunTests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sbatch Sequential.sh 512 512
-sbatch Static.sh 3 512 512
-sbatch Dynamic.sh 3 512 512
-sbatch Static.sh 9 512 512
-sbatch Dynamic.sh 9 512 512
-sbatch Static.sh 13 512 512
-sbatch Dynamic.sh 13 512 512
+sbatch -n 3 Static.sh 512 512
+sbatch -n 3 Dynamic.sh 512 512
+sbatch -n 9 Static.sh 9 512 512
+sbatch -n 9 Dynamic.sh 9 512 512
+sbatch -n 13 Static.sh 512 512
+sbatch -n 13 Dynamic.sh 512 512
 sbatch Static.sh 16 512 512
 sbatch Dynamic.sh 16 512 512
 sbatch Static.sh 17 512 512
@@ -15,12 +15,12 @@ sbatch Dynamic.sh 32 512 512
 sbatch Static.sh 33 512 512
 sbatch Dynamic.sh 33 512 512
 sbatch Sequential.sh 2048 2048
-sbatch Static.sh 3 2048 2048
-sbatch Dynamic.sh 3 2048 2048
-sbatch Static.sh 9 2048 2048
-sbatch Dynamic.sh 9 2048 2048
-sbatch Static.sh 13 2048 2048
-sbatch Dynamic.sh 13 2048 2048
+sbatch -n 3 Static.sh 2048 2048
+sbatch -n 3 Dynamic.sh 2048 2048
+sbatch -n 9 Static.sh 2048 2048
+sbatch -n 9 Dynamic.sh 2048 2048
+sbatch -n 13 Static.sh 2048 2048
+sbatch -n 13 Dynamic.sh 2048 2048
 sbatch Static.sh 16 2048 2048
 sbatch Dynamic.sh 16 2048 2048
 sbatch Static.sh 17 2048 2048
@@ -30,12 +30,12 @@ sbatch Dynamic.sh 32 2048 2048
 sbatch Static.sh 33 2048 2048
 sbatch Dynamic.sh 33 2048 2048
 sbatch Sequential.sh 8192 8192
-sbatch Static.sh 3 8192 8192
-sbatch Dynamic.sh 3 8192 8192
-sbatch Static.sh 9 8192 8192
-sbatch Dynamic.sh 9 8192 8192
-sbatch Static.sh 13 8192 8192
-sbatch Dynamic.sh 13 8192 8192
+sbatch -n 3 Static.sh 8192 8192
+sbatch -n 3 Dynamic.sh 8192 8192
+sbatch -n 9 Static.sh 8192 8192
+sbatch -n 9 Dynamic.sh 8192 8192
+sbatch -n 13 Static.sh 8192 8192
+sbatch -n 13 Dynamic.sh 8192 8192
 sbatch Static.sh 16 8192 8192
 sbatch Dynamic.sh 16 8192 8192
 sbatch Static.sh 17 8192 8192
@@ -45,10 +45,10 @@ sbatch Dynamic.sh 32 8192 8192
 sbatch Static.sh 33 8192 8192
 sbatch Dynamic.sh 33 8192 8192
 sbatch Sequential.sh 16384 16384
-sbatch Static.sh 3 16384 16384
-sbatch Dynamic.sh 3 16384 16384
-sbatch Static.sh 9 16384 16384
-sbatch Dynamic.sh 9 16384 16384
+sbatch -n 3 Static.sh 16384 16384
+sbatch -n 3 Dynamic.sh 16384 16384
+sbatch -n 9 Static.sh 16384 16384
+sbatch -n 9 Dynamic.sh 16384 16384
 sbatch Static.sh 13 16384 16384
 sbatch Dynamic.sh 13 16384 16384
 sbatch Static.sh 16 16384 16384
@@ -60,10 +60,10 @@ sbatch Dynamic.sh 32 16384 16384
 sbatch Static.sh 33 16384 16384
 sbatch Dynamic.sh 33 16384 16384
 sbatch Sequential.sh 32768 32768
-sbatch Static.sh 3 32768 32768
-sbatch Dynamic.sh 3 32768 32768
-sbatch Static.sh 9 32768 32768
-sbatch Dynamic.sh 9 32768 32768
+sbatch -n 3 Static.sh 32768 32768
+sbatch -n 3 Dynamic.sh 32768 32768
+sbatch -n 9 Static.sh 32768 32768
+sbatch -n 9 Dynamic.sh 32768 32768
 sbatch Static.sh 13 32768 32768
 sbatch Dynamic.sh 13 32768 32768
 sbatch Static.sh 16 32768 32768
