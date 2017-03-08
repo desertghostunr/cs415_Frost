39a5bb9b146e15246b287c2fea709e3f1e440d2e
= <=>
2017-03-07 21:56:58 -0800

    Processed the test data.

--- PA2/build/PD33.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -n 33
-#SBATCH --mem=2048
-#SBATCH --time=00:10:00
-
-srun PA2_Parallel_Dynamic 16384 16384 images/parallel_dd.pgm
-
--- PA2/build/PS29.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -n 29
-#SBATCH --mem=2048
-#SBATCH --time=00:10:00
-
-srun PA2_Parallel_Static 16384 16384 images/parallel_ss.pgm
-
