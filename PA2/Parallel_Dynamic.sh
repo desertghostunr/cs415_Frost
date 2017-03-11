cb4305b45798dca39f3fd4ea0affeba6e28ace4a
= <=>
2017-03-10 16:45:31 -0800

    Need to run filler tests for errors

--- PA2/Parallel_Dynamic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -n 8
-#SBATCH --mem=2048
-#SBATCH --time=00:10:00
-
-srun PA2_Parallel_Dynamic 512 512 parallel_d.pgm
-
--- PA2/Sequential.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -n 1
-#SBATCH --mem=2048
-#SBATCH --time=00:10:00
-
-./PA2_Sequential 512 512 sequential.pgm
