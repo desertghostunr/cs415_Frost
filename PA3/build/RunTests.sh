5fa27665dca411846a9173e614499ffbcef66362
Andrew Frost <desertghost@nevada.unr.edu>
2017-03-29 14:17:18 -0700

    Fixed tests.

--- PA3/build/RunTests.sh
+++ PA3/build/RunTests.sh
@@ -10,15 +10,20 @@ for (( i=10; i<=$n;i=5*i ))
 do
 	echo "Generating $i numbers"
 
-	./generator "$i" > ../bin/data"$i".txt
+	srun generator "$i" > ../bin/data"$i".txt
 done
 
+for (( j = 0; j < 5; j++ ))
 
-for (( i=10; i<=$n;i=5*i ))
-do
-	echo "Running test of $i numbers"
+	for (( i=10; i<=$n;i=5*i ))
+	do
+		echo "Running test of $i numbers"
 
-	sbatch Sequential.sh ../bin/data"$i".txt
+		sbatch Sequential.sh ../bin/data"$i".txt
 
-	sleep 2.5
+		sleep 2.5
+	done
+	
+	sleep 60
+	
 done
