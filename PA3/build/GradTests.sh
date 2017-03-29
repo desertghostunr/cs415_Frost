5c03fcf47bceb6b5f5ced01d8cd3380e31ce12d5
= <=>
2017-03-29 16:07:05 -0700

    Re-ran Tests!

--- PA3/build/GradTests.sh
+++ PA3/build/GradTests.sh
@@ -10,11 +10,11 @@ for (( i=400000; i<=$n;i+=4980000 ))
 do
 	echo "Generating $i numbers"
 
-	srun n1 generator "$i" > ../bin/data"$i".txt
+	./generator "$i" > ../bin/data"$i".txt
 done
 
 for (( j=0; j<5; j++ ))
-
+do
 	for (( i=400000; i<=$n;i+=4980000 ))
 	do
 		echo "Running test of $i numbers"
@@ -24,6 +24,6 @@ for (( j=0; j<5; j++ ))
 		sleep 20
 		squeue
 	done
-	
+
 	sleep 120
 done
--- PA3/build/RunTests.sh
+++ PA3/build/RunTests.sh
@@ -10,11 +10,13 @@ for (( i=10; i<=$n;i=5*i ))
 do
 	echo "Generating $i numbers"
 
-	srun n1 generator "$i" > ../bin/data"$i".txt
+	./generator "$i" > ../bin/data"$i".txt
+	sleep 5
+
 done
 
 for (( j = 0; j < 5; j++ ))
-
+do
 	for (( i=10; i<=$n;i=5*i ))
 	do
 		echo "Running test of $i numbers"
@@ -22,8 +24,9 @@ for (( j = 0; j < 5; j++ ))
 		sbatch Sequential.sh ../bin/data"$i".txt
 
 		sleep 2.5
+		squeue
 	done
-	
+
 	sleep 60
-	
+
 done
