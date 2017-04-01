bb49e6e56241390ebff256705473165e25524d32
= <=>
2017-04-01 14:32:31 -0700

    Wrote the driver portion of the parallel sort... need to write the actual parallel sort method...

--- PA3/src/Parallel.cpp
+++ PA3/src/Parallel.cpp
@@ -18,7 +18,6 @@
 // header files //////////////////////////////////////////////
 #include <iostream>
 #include <vector>
-#include <sstream>
 #include <string>
 #include <fstream>
 #include <cmath>
@@ -30,6 +29,7 @@
 #define SAVE_FLAG 1
 #define SAVE_CODE -1
 #define SEND_TIME_CODE -101
+#define SEND_DATA -201
 
 // main /////////////////////////////////////////////////////
 int main( int argc, char *argv[ ] )
@@ -38,10 +38,10 @@ int main( int argc, char *argv[ ] )
     unsigned long long sTime, eTime;
     double tTime, finalTime;
     std::vector< int > data;
-    int min, max, tmpInt, saveFlag = 0;
-    int index;
+    int tmpInt, amntOfData, saveFlag = 0;
+    int minMax[2];
+    int index, dIndex, rmndr;
     std::string fileName;
-    std::stringstream strStream;
     std::fstream file;
     size_t extensionPos;
 
@@ -77,61 +77,105 @@ int main( int argc, char *argv[ ] )
 
         if( !file.is_open( ) )
         {
-            std::cout << "Error: unable to open " << fileName << "." << std::endl;
-            return -1;
+            std::cout << "Error: unable to open " << fileName << "." << std::endl;            
         }
     
         //read in data
-        strStream.str( std::string( std::istreambuf_iterator<char>( file ),
-                                std::istreambuf_iterator<char>( ) ) );
-        file.close( );
-
-        //process the data
-        if( !( strStream >> tmpInt ) )
+        if( !(file >> tmpInt ) )
         {
-            std::cout << "Error: Invalid data in file!" << std::endl;
-            return -1;
+            std::cout << "Warning: Corrupted file contents detected." << std::endl;
         }
 
-        data.resize( tmpInt );
+        data.resize( tmpInt / numberOfTasks, 0 );
+
+        //check if the data is divided even by the number of processes
+        if( tmpInt % numberOfTasks == 0 )
+        {
+            rmndr = 0;
+        }
+        else
+        {
+            rmndr = 1;
+        }
+
+        amntOfData = tmpInt;
     
         max = -1 * std::numeric_limits<int>::infinity( );
         min = std::numeric_limits<int>::infinity( );
 
+        //read in data and send it to the slaves
+        for( index = 1; index < numberOfTasks; index++ )
+        {
+            for( dIndex = 0; dIndex < static_cast<int>( data.size( ) ); dIndex++ )
+            {
+                if( !(file >> tmpInt ) )
+                {
+                    std::cout << "Warning: Corrupted file contents detected." << std::endl;
+                }
+
+                if( tmpInt < minMax[ 0 ] )
+                {
+                    minMax[ 0 ] = tmpInt;
+                }
+
+                if( tmpInt > minMax[ 1 ] )
+                {
+                    minMax[ 1 ] = tmpInt;
+                }
+
+                data[ dIndex ] = tmpInt;
+            }
+
+            MPI_Send( &amntOfData, 1, MPI_INT, index, SEND_DATA + 1, MPI_COMM_WORLD ); //send amntOfData
+            MPI_Send( &data[ 0 ], static_cast<int>( data.size( ) ), MPI_INT, index, SEND_DATA, MPI_COMM_WORLD ); //send data          
+        }
+
+        data.resize( data.size( ) + rmndr ); //resize for any remainder
+
+        //read in data for master
         for( index = 0; index < static_cast<int>( data.size( ) ); index++ )
         {
-            if( !( strStream >> tmpInt ) )
+            if( !(file >> tmpInt ) )
             {
-                std::cout << "Error: Invalid data in file!" << std::endl;
+                std::cout << "Warning: Corrupted file contents detected." << std::endl;
             }
 
-            if( tmpInt < min )
+            if( tmpInt < minMax[ 0 ] )
             {
-                min = tmpInt;
+                minMax[ 0 ] = tmpInt;
             }
 
-            if( tmpInt > max )
+            if( tmpInt > minMax[ 1 ] )
             {
-                max = tmpInt;
+                minMax[ 1 ] = tmpInt;
             }
 
             data[ index ] = tmpInt;
         }
+
+        file.close( );
+
+        //send the min and the max to each slave
+        for( index = 1; index < numberOfTasks; index++ )
+        {
+            MPI_Send( &minMax[ 0 ], 2, MPI_INT, index, SEND_DATA + 2, MPI_COMM_WORLD );
+        }
     }
-    else //slaves
+    else //slaves, get data for each slave
     {
+        MPI_Recv( &amntOfData, 1, MPI_INT, 0, SEND_DATA + 1, MPI_COMM_WORLD, &status ); //get total amntOfData
+        data.resize( amntOfData / numberOfTasks, 0 ); //alloc for data
+        MPI_Recv( &data[ 0 ], static_cast<int>( data.size( ) ), MPI_INT, 0, SEND_DATA, MPI_COMM_WORLD, &status ); //get data
 
+        MPI_Recv( &minMax[ 0 ], 2, MPI_INT, 0, SEND_DATA + 2, MPI_COMM_WORLD, &status );
     }
 
-    
+    //wait for all processes to be ready
+    MPI_Barrier( MPI_COMM_WORLD );    
 
-    
-
-    
-
+    //sort the data
     sTime = GetCurrentMicroSecTime( );
-
-    //sort
+    
     tSort::sBucket( data, numberOfTasks, min, max );
 
     eTime = GetCurrentMicroSecTime();
@@ -159,7 +203,7 @@ int main( int argc, char *argv[ ] )
     }
 
 
-    //get whether or not to save
+    //get whether or not to save ////////////////////////////////////////////
     if( argc > 2 )
     {
         strStream.str( std::string( "" ) );
@@ -170,7 +214,7 @@ int main( int argc, char *argv[ ] )
         strStream >> saveFlag;
     }
     
-    //write out data
+    //write out data ///////////////////////////////////////////////////////
     if( saveFlag )
     {
         if( taskID == 0 )
@@ -230,13 +274,15 @@ int main( int argc, char *argv[ ] )
         
     }    
 
-    //print the time
+    //print the time ///////////////////////////////////////////////////////////////////
 
     if( taskID == 0 )
     {
         std::cout<<"Parallel\t"<<numberOfTasks<<"\t";
         std::cout<<data.size()<<"\t"<<finalTime<<std::endl;
     }
+
+   // finalize ///////////////////////////////////////////////////////////////////////// 
     
     MPI_Finalize();
 
