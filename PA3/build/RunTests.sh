b66598e36a5d0a5928580fd97427b296c9fd2b94
Andrew Frost <desertghost@nevada.unr.edu>
2017-04-03 11:04:29 -0700

    Fixed the sequential tests. Commented out the parallel tests.

--- PA3/build/RunTests.sh
+++ PA3/build/RunTests.sh
@@ -20,45 +20,51 @@ do
 done
 
 #sequential
-#for (( j = 0; j < 5; j++ ))
-#do
-#	for (( i=10; i<=$n;i=5*i ))
-#	do
-#		echo "Running test of $i numbers"
-
-#		sbatch Sequential.sh ../bin/data"$i".txt
-
-#		sleep 2.5
-#		squeue
-#	done
-
-#	sleep 60
-
-#done
-
-#parallel
 for (( j = 0; j < 5; j++ ))
 do
 	for (( i=10; i<=$n;i=5*i ))
 	do
 		for (( k=2; k<=32;k=2*k ))
 		do
-			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
+			echo "Running test of $i numbers"
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+			sbatch Sequential.sh ../bin/data"$i".txt "$k"
 
-			sleep 25
+			sleep 30
 			squeue
 		done
-		sleep 35
+		
+		sleep 60
+		squeue
 	done
 
-	sleep 120
+	sleep 60
 
 done
 
+#parallel
+#for (( j = 0; j < 5; j++ ))
+#do
+#	for (( i=10; i<=$n;i=5*i ))
+#	do
+#		for (( k=2; k<=32;k=2*k ))
+#		do
+#			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
+
+#			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+
+#			sleep 25
+#			squeue
+#		done
+#		sleep 35
+#	done
+
+#	sleep 120
+
+#done
+
 echo "Pausing before running gradual tests"
-sleep 120
+sleep 180
 
 
 
@@ -76,37 +82,43 @@ do
 done
 
 #sequential tests
-#for (( j=0; j<5; j++ ))
-#do
-#	for (( i=400000; i<=$n;i+=4980000 ))
-#	do
-#		echo "Running test of $i numbers"
-
-#		sbatch Sequential.sh ../bin/data"$i".txt
-
-#		sleep 20
-#		squeue
-#	done
-
-#	sleep 120
-#done
-
-#parallel tests
-for (( j = 0; j < 5; j++ ))
+for (( j=0; j<5; j++ ))
 do
 	for (( i=400000; i<=$n;i+=4980000 ))
 	do
 		for (( k=2; k<=32;k=2*k ))
 		do
-			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
+			echo "Running test of $i numbers"
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+			sbatch Sequential.sh ../bin/data"$i".txt "$k"
 
-			sleep 20
+			sleep 30
 			squeue
 		done
-		sleep 45
+		
+		sleep 60
+		squeue
 	done
+
 	sleep 120
 done
+
+#parallel tests
+#for (( j = 0; j < 5; j++ ))
+#do
+#	for (( i=400000; i<=$n;i+=4980000 ))
+#	do
+#		for (( k=2; k<=32;k=2*k ))
+#		do
+#			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
+
+#			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+
+#			sleep 20
+			squeue
+#		done
+#		sleep 45
+#	done
+#	sleep 120
+#done
 
