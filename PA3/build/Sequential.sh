785b29531090232b4ab5060614ffbb5aaeff233d
= <=>
2017-04-03 10:54:36 -0700

    Fixed Sequential.sh

--- PA3/build/Sequential.sh
+++ PA3/build/Sequential.sh
@@ -2,6 +2,7 @@
 
 file=$1
 buckets=$2
+save=$3
 
 if ! [[ -n "$file" ]]; then
 	file="../bin/data.txt"
@@ -11,9 +12,13 @@ if ! [[ -n "$buckets" ]]; then
 	buckets="-1"
 fi
 
+if ! [[ -n "$save" ]]; then
+	save="0"
+fi
+
 #SBATCH -n 1
 #SBATCH --mem=2048
 #SBATCH --time=00:05:00
 #SBATCH -o ../bin/sequential-%j.out
 
-./Sequential "$file" "$buckets"
+./Sequential "$file" "$buckets" "$save"
