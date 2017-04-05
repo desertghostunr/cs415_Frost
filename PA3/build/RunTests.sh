09ebb80c93529718784348d9603b83db30810cd3
= <=>
2017-04-04 21:36:41 -0700

    Updated run script

--- PA3/build/RunTests.sh
+++ PA3/build/RunTests.sh
@@ -3,97 +3,117 @@
 n=$1
 
 if ! [[ -n "$n" ]]; then
-	n="100000000"
+	n="1000000000"
 fi
 
 #sequential
 for (( j = 0; j < 4; j++ ))
 do
-	for (( i=10; i<=$n;i=5*i ))
+	for (( i=10; i<=$n;i=10*i ))
 	do
-		for (( k=2; k<=32;k=2*k ))
+		echo "Running test of $i numbers"
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
 		do
-			echo "Running test of $i numbers"
-
-			sbatch Sequential.sh ../bin/data"$i".txt "$k"
-
-			sleep 30
-			squeue
-		done
+			sleep 2s
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done		
 		
-		sleep 60
-		sbatch Sequential.sh ../bin/data"$i".txt
-		squeue
+		sbatch Sequential.sh "$i"
+
+		sleep 2s
 	done
-
-	sleep 60
-
 done
 
 #parallel
 for (( j = 0; j < 5; j++ ))
 do
-	for (( i=10; i<=$n;i=5*i ))
+	for (( i=10; i<=$n;i=10*i ))
 	do
 		for (( k=2; k<=32;k=2*k ))
 		do
 			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			while [["$TEST"=~"andrewfr"]]
+			do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			done
 
-			sleep 25
-			squeue
+			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh "$i"
+
+			sleep 2s
+
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			while [["$TEST"=~"andrewfr"]]
+			do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			done
+
+			sbatch -N "$(( (($k - 1) / 8) + 2 ))" -n "$k" Parallel.sh "$i"
+			
 		done
-		sleep 35
+		sleep 2s
 	done
 
-	sleep 120
+	sleep 2s
 
 done
-
-echo "Pausing before running gradual tests"
-sleep 180
-
 
 #sequential tests
 for (( j=0; j<5; j++ ))
 do
-	for (( i=400000; i<=$n;i+=4980000 ))
+	for (( i=200000; i<=2000000000;i=10*i))
 	do
-		for (( k=2; k<=32;k=2*k ))
+		echo "Running test of $i numbers"
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
 		do
-			echo "Running test of $i numbers"
-
-			sbatch Sequential.sh ../bin/data"$i".txt "$k"
-
-			sleep 30
-			squeue
-		done
+			sleep 2s
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done		
 		
-		sleep 60
-		sbatch Sequential.sh ../bin/data"$i".txt
-		squeue
+		sbatch Sequential.sh "$i"
+
+		sleep 2s
 	done
 
-	sleep 120
+	sleep 2s
 done
 
 #parallel tests
 for (( j = 0; j < 5; j++ ))
 do
-	for (( i=400000; i<=$n;i+=4980000 ))
+	for (( i=200000; i<=2000000000;i=10*i))
 	do
 		for (( k=2; k<=32;k=2*k ))
 		do
 			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh ../bin/data"$i".txt
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			while [["$TEST"=~"andrewfr"]]
+			do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			done
 
-			sleep 20
-			squeue
+			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh "$i"
+
+			sleep 2s
+
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			while [["$TEST"=~"andrewfr"]]
+			do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+			done
+
+			sbatch -N "$(( (($k - 1) / 8) + 2 ))" -n "$k" Parallel.sh "$i"
 		done
-		sleep 45
+		sleep 2s
 	done
-	sleep 120
+	sleep 2s
 done
 
