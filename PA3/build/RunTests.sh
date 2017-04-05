407d80b6ae5d4651b80850de9ec2dd668a3fe6ad
= <=>
2017-04-04 21:47:04 -0700

    Fixed RunTests

--- PA3/build/RunTests.sh
+++ PA3/build/RunTests.sh
@@ -30,7 +30,7 @@ for (( j = 0; j < 5; j++ ))
 do
 	for (( i=10; i<=$n;i=10*i ))
 	do
-		for (( k=2; k<=32;k=2*k ))
+		for (( k=2; k<=16;k=2*k ))
 		do
 			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
 
@@ -41,7 +41,7 @@ do
 				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
 			done
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh "$i"
+			sbatch -n "$k" Parallel.sh "$i"
 
 			sleep 2s
 
@@ -52,9 +52,32 @@ do
 				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
 			done
 
-			sbatch -N "$(( (($k - 1) / 8) + 2 ))" -n "$k" Parallel.sh "$i"
+			sbatch -n "$(($k+1))" Parallel.sh "$i"
 			
 		done
+		sleep 2s
+
+		echo "Running test of $i numbers and 24 tasks"
+
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
+		do
+			sleep 2s
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done
+
+		sbatch -n "24" Parallel.sh "$i"
+
+		sleep 2s
+
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
+		do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done
+
+		sbatch -n "25" Parallel.sh "$i"
 		sleep 2s
 	done
 
@@ -88,7 +111,7 @@ for (( j = 0; j < 5; j++ ))
 do
 	for (( i=200000; i<=2000000000;i=10*i))
 	do
-		for (( k=2; k<=32;k=2*k ))
+		for (( k=2; k<=16;k=2*k ))
 		do
 			echo "Running test of $i numbers and $k tasks on $(( (($k - 1) / 8) + 1 )) boxes"
 
@@ -99,7 +122,7 @@ do
 				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
 			done
 
-			sbatch -N "$(( (($k - 1) / 8) + 1 ))" -n "$k" Parallel.sh "$i"
+			sbatch -n "$k" Parallel.sh "$i"
 
 			sleep 2s
 
@@ -110,8 +133,32 @@ do
 				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
 			done
 
-			sbatch -N "$(( (($k - 1) / 8) + 2 ))" -n "$k" Parallel.sh "$i"
+			sbatch -n "$(($k+1))" Parallel.sh "$i"
+			
 		done
+		sleep 2s
+
+		echo "Running test of $i numbers and 24 tasks"
+
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
+		do
+			sleep 2s
+			TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done
+
+		sbatch -n "24" Parallel.sh "$i"
+
+		sleep 2s
+
+		TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		while [["$TEST"=~"andrewfr"]]
+		do
+				sleep 2s
+				TEST=$(squeue -o"%.18i %.9P %.20j %.20u %.8T %.10M %.9l %.6D %R")
+		done
+
+		sbatch -n "25" Parallel.sh "$i"
 		sleep 2s
 	done
 	sleep 2s
